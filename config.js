b5a63f56b899d042eb1462c2a23accc97cda5ee3
huyconn <108627467+huyconn@users.noreply.github.com>
2022-07-05 01:17:18 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,5 +11,5 @@ const CONFIG = {
     mess: 'T biết, m cũng là người đặc biệt mà <3',
     messDesc: 'Mình tìm hỉu nheee :P',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100026500275555' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://huyconn.github.io/WomenDay/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
