a590a8f79eb8b315db0b63f19a8ee1b13a4f718d
huyconn <108627467+huyconn@users.noreply.github.com>
2022-07-05 00:36:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,22 +1,15 @@
 const CONFIG = {
     introTitle: 'Cậu ơi!',
-    introDesc: `Trái đất vốn lạ thường
-    
-    Mà sao em cứ đi nhầm đường
-    
-    Lạc vào tim anh lẻ loi
-    
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Tớ thích cậuuuu',
-    btnNo: 'Không, tớ chỉ coi cậu là bạn :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại thích tớ <3',
-    btnReply: 'Gửi cho tớ <3',
-    reply: 'Phải iuuu',
-    mess: 'Tớ biết mà, tớ cũng thích cậuuuuuu <3',
-    messDesc: 'Mình hẹn hò nheee :P',
+    btnIntro: 'Gì thế',
+    title: 'Cậu có muốn gần tớ hơn không',
+    desc: ' ',
+    btnYes: 'Tớ muốnnnn',
+    btnNo: 'Không muốnnn :3',
+    question:'Vì sao cậu muốn gần tớ hơn <3',
+    btnReply: 'Gửi <3',
+    reply: ' Vì cậu đặc biệt ',
+    mess: 'Tớ biết, cậu cũng là người đặc biệt mà <3',
+    messDesc: 'Mình tìm hỉu nheee :P',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100026500275555' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
